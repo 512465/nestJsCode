8ecc719fefdf24122b1db1cf751bb908c533fd0d
王林 <1570898673@qq.com>
2024-09-07 01:29:09 +0800

    数据库配置和swagger文档配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ArticleModule } from './article/article.module';
 import { UserModule } from './user/user.module';
 import { DetailsModule } from './details/details.module';
 import { ActivityModule } from './activity/activity.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,18 @@ import { ActivityModule } from './activity/activity.module';
     UserModule,
     DetailsModule,
     ActivityModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 数据库类型
+      host: 'localhost', // 数据库地址
+      port: 3306, // 数据库端口
+      username: 'root', // 数据库用户名
+      password: '123456', // 数据库密码
+      database: 'codepaint', // 数据库名称
+      synchronize: true, // 是否自动创建数据库表
+      retryDelay: 500, // 重试间隔时间
+      retryAttempts: 10, // 重试次数
+      autoLoadEntities: true, // 自动加载实体
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('工作室接口文档')
+    .setDescription('工作室后端接口测试文档')
+    .setVersion('1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
