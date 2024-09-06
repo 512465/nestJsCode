8931164a7d527c8a9164650249e1cba537ad1831
王林 <1570898673@qq.com>
2024-09-07 00:39:25 +0800

    创建模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { ArticleModule } from './article/article.module';
+import { UserModule } from './user/user.module';
+import { DetailsModule } from './details/details.module';
+import { ActivityModule } from './activity/activity.module';
 
 @Module({
-  imports: [],
+  imports: [LoginModule, ArticleModule, UserModule, DetailsModule, ActivityModule],
   controllers: [AppController],
   providers: [AppService],
 })
