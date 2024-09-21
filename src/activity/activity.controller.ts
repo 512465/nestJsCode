972edb0d88b4b15c1d6ee536004245d025284faa
hot777zz <912275990@qq.com>
2024-09-21 19:19:14 +0800

    lh:活动接口完成

--- src/activity/activity.controller.ts
+++ src/activity/activity.controller.ts
@@ -1,17 +1,50 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Delete } from '@nestjs/common';
 import { ActivityService } from './activity.service';
-import { CreateActivityDto } from './dto/create-activity.dto';
+import { Activity } from './entities/activity.entity';
+import { UploadService } from 'src/upload/upload.service';
 @Controller('activity')
 export class ActivityController {
-  constructor(private readonly activityService: ActivityService) {}
+  constructor(
+    private readonly activityService: ActivityService,
+    private readonly uploadService: UploadService,
+  ) {}
 
   @Get()
-  findAll() {
-    return this.activityService.findAll();
+  async getDetailsWithPagination(
+    @Query('page') page,
+    @Query('pageSize') pageSize,
+  ): Promise<{ data: Activity[]; total: number }> {
+    return this.activityService.getactivityWithPagination(page, pageSize);
+  }
+  @Post()
+  async create(
+    @Body('title') title: string,
+    @Body('content') content: string,
+    @Body('intro') intro: string,
+    @Body('picture') picture: string,
+    @Body('creator') creator: string,
+  ) {
+    const createActivityDto = {
+      title,
+      content,
+      intro,
+      picture, // 使用上传后的文件路径
+      creator,
+    };
+    return this.activityService.create(createActivityDto);
   }
 
-  @Post()
-  create(@Body() createActivityDto: CreateActivityDto) {
-    return this.activityService.create(createActivityDto);
+  @Get('detail')
+  getDetails(@Query('id') id: number) {
+    return this.activityService.getDetail(id);
+  }
+
+  @Get('recent')
+  getRecent() {
+    return this.activityService.getRecent();
+  }
+  @Delete()
+  delete(@Query('id') id: number) {
+    return this.activityService.delete(id);
   }
 }
--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -8,11 +8,26 @@ export class ActivityService {
     @InjectRepository(Activity)
     private readonly ActivityRepository: Repository<Activity>,
   ) {}
-  findAll() {
-    return this.ActivityRepository.find();
+  async getactivityWithPagination(page: number = 1, pageSize: number = 10) {
+    const [data, total] = await this.ActivityRepository.findAndCount({
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+      select: {
+        id: true,
+        title: true,
+        picture: true,
+        creator: true,
+        intro: true,
+        // 明确排除 content 字段
+      },
+    });
+    return {
+      data,
+      total,
+    };
   }
 
-  create(createActivityDto: any) {
+  async create(createActivityDto: any) {
     if (!createActivityDto) {
       throw new Error('参数错误');
     }
@@ -20,5 +35,33 @@ export class ActivityService {
       createActivityDto.creator = 'CodePaint';
     }
     return this.ActivityRepository.save(createActivityDto);
+  }
+
+  async getDetail(id: number) {
+    if (!id) {
+      throw new Error('参数错误');
+    }
+    return this.ActivityRepository.findOneBy({ id });
+  }
+
+  async getRecent(): Promise<Activity> {
+    const recentActivity = await this.ActivityRepository.find({
+      order: { createdAt: 'DESC' }, // 按照 createdAt 字段降序排列
+      take: 1, // 只取一条记录
+      select: {
+        id: true,
+        title: true,
+        picture: true,
+        // 明确排除 content 字段
+      },
+    });
+
+    return recentActivity[0]; // 返回第一个活动
+  }
+  async delete(id: number) {
+    if (!id) {
+      throw new Error('参数错误');
+    }
+    return this.ActivityRepository.delete(id);
   }
 }
